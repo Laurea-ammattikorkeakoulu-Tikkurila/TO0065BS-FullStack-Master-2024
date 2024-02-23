71e1abb9da530cb3df4073204e46bd852479ae06
Jari Kovalainen <itse@jarikovalainen.com>
2024-02-23 08:31:14 +0000

    WS03 päivitys

--- WS02_Files_Directories/tuntitehtävä/01_Serve_file_sync.js
+++ WS02_Files_Directories/tuntitehtävä/01_Serve_file_sync.js
@@ -1,15 +1,18 @@
+// haetaan tarvittavat moduulit
 var http = require("http");
 var fs = require("fs");
+// luodaan palvelinportti
 const PORT = process.env.PORT || 3001;
 
 //create a server object:
 const server = http.createServer(function (request, response) {
+    // File reading with synchronous method
     try {
         const data = fs.readFileSync('./files/example.txt', 'utf8');
         response.writeHead(200, { "Content-Type": "text/plain" });
         response.write(data);
         response.end(); //end the response
-
+        // jos virhe, tulostetaan virheilmoitus konsoliin ja palautetaan 404 Not Found
     } catch (err) {
         console.error(err);
         response.writeHead(404, { "Content-Type": "text/plain" });
@@ -17,4 +20,5 @@ const server = http.createServer(function (request, response) {
         response.end(); //end the response
     }
 });
+// palvelin kuuntelee porttia PORT
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- WS02_Files_Directories/tuntitehtävä/02_Serve_file_async.js
+++ WS02_Files_Directories/tuntitehtävä/02_Serve_file_async.js
@@ -1,20 +1,27 @@
+// haetaan tarvittavat moduulit
 var http = require("http");
 var fs = require("fs/promises");
+// haetaan polkumoduuli, joka mahdollistaa tiedostojen käsittelyn
 const path = require("path");
+// luodaan palvelinportti
 const PORT = process.env.PORT || 3002;
 
-
+//create a server object:
 const server = http.createServer(function (request, response) {
-    const filePath = path.join(__dirname, "files", "example.txt");
-    // console.log(filePath);
+    //assign the file path to a variable
+    const filePath = path.join(__dirname, "files", "example2.tt");
+    // console.log(filePath)
+    // File reading with asynchronous method;
     async function serveFile(filePath, response) {
+        // luetaan tiedosto
         try {
+            // assign the file data to a variable in an asynchronous way
             const data = await fs.readFile(filePath, 'utf-8');
             //           console.log(data);
             response.writeHead(200, { "Content-Type": "text/plain" });
             response.write(data);
             response.end(); //end the response
-
+            // jos virhe, tulostetaan virheilmoitus konsoliin ja palautetaan 404 Not Found
         } catch (err) {
             console.error(err);
             response.writeHead(404, { "Content-Type": "text/plain" });
@@ -22,6 +29,9 @@ const server = http.createServer(function (request, response) {
             response.end(); //end the response  
         }
     }
+    // kutsutaan funktiota jolla palvellaan tiedosto tai palautetaan virheilmoitus
     serveFile(filePath, response);
 });
+
+// palvelin kuuntelee porttia PORT
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
