db427d389f72190545c4e3482c21e8c347e3ba90
Jari Kovis <itse@jarikovalainen.com>
2024-04-02 20:05:32 +0300

    Mongoose tavaaraa

--- WS09_mongoose/03_schema.js
+++ WS09_mongoose/03_schema.js
@@ -1,12 +1,12 @@
 // Otetaan moduuli käyttöön
 require("dotenv").config();
 var mongoose = require("mongoose");
+
 const Kitten = require("./modules/model");
 // Määritellään yhteysosoite
 var user = process.env.DB_USER
 var salis = process.env.DB_PASS
 const uri = process.env.MONGODB_URI;
-
 
 // Luodaan yhteys
 
